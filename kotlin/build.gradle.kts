0524b84a7b9f0ace9e161fe7f6b88e9f4dbf157b
MOSIN SHAIKH <mosin505@gmail.com>
2026-09-22 00:56:34 +0530

    Start Phase 10 Kotlin migration: kotlin/build.gradle.kts

---
+++ kotlin/build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    kotlin("jvm") version "2.4.20" apply false
+    id("org.jetbrains.compose") version "1.12.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.20" apply false
+}
